b11de5b41c14c04d875fd6030725e56bf39227d8
Alexander Soroka <soroka.a.m@gmail.com>
2013-03-16 14:10:07 +0300

    Identation was fixed. New line at the end of file was fixed. Include guard was fixed

--- include/common/disallow_copy_and_assign.h
+++ include/common/disallow_copy_and_assign.h
@@ -1,10 +1,10 @@
-#ifndef K52_INCLUDE_COMMON_DISALLOW_COPY_AND_ASSIGN_H
-#define K52_INCLUDE_COMMON_DISALLOW_COPY_AND_ASSIGN_H
+#ifndef K52_DISALLOW_COPY_AND_ASSIGN_H
+#define K52_DISALLOW_COPY_AND_ASSIGN_H
 
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
-  TypeName(const TypeName&);               \
-  void operator=(const TypeName&)
+    TypeName(const TypeName&);               \
+    void operator=(const TypeName&)
 
-#endif
+#endif //
