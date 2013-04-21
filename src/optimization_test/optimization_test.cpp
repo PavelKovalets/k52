f53160cc6400ee391f1173ad2db84ad53fdfbee4
Feanor <feanor-pk@yandex.ru>
2013-04-21 12:11:40 +0300

    Optimization tests linked with k52

--- src/optimization_test/optimization_test.cpp
+++ src/optimization_test/optimization_test.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
+#include <k52/optimization/bounded_nelder_mead.h>
+
 using namespace std;
 
 int main()
 {
     cout<<"Hello from optimization tests"<<endl;
+    double l = 10;
+    double precision = 1e-9;
+    double lower_bound = -10000;
+    double upper_bound = 10000;
+    k52::optimization::BoundedNelderMead bounded_nelder_mead(l, precision, lower_bound, upper_bound);
+    k52::optimization::IOptimizer* optimizer = &bounded_nelder_mead;
     return 0;
 }
