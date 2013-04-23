04f46e669907894c7ed042ced0eb8f5a9b5c3991
Alexander Soroka <soroka.a.m@gmail.com>
2013-04-23 10:52:33 +0300

    Mpi configuration logic has been improved.

--- configuration/mpi-config.cmake
+++ configuration/mpi-config.cmake
@@ -3,13 +3,15 @@ set(K52_MPI_LIBRARY_PATH "")
 set(K52_MPI_LIBRARIES "")
 
 find_package(MPI)
-if(MPI_FOUND)
+if(MPI_CXX_FOUND)
   message("Mpi has been found")
 
   if(Boost_MPI_FOUND AND Boost_SERIALIZATION_FOUND)
     message("Using mpi")
     add_definitions(-DBUILD_WITH_MPI)
+    include_directories(${MPI_CXX_INCLUDE_PATH})
+    set(K52_MPI_LIBRARIES ${MPI_CXX_LIBRARIES})
   endif(Boost_MPI_FOUND AND Boost_SERIALIZATION_FOUND)
 
-endif(MPI_FOUND)
+endif(MPI_CXX_FOUND)
 
