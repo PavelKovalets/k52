caca229dbd5e35acfec6e201520a2c167b658a55
Feanor <feanor-pk@yandex.ru>
2013-07-30 21:45:04 +0300

    Enable vagrant powered VM

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+apt-get update -y
+apt-get install -y openmpi-bin
+apt-get install -y libboost-dev
+apt-get install -y libboost-mpi-dev
+apt-get install -y libboost-thread-dev
+apt-get install -y build-essential
+apt-get install -y cmake
