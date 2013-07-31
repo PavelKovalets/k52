b55f2f5d33f61767bee82cb79f55a16531b91cf5
FeanorPk <feanor-pk@yandex.by>
2013-07-31 10:05:56 +0000

    bootstrap.sh to download libboost-all-dev

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,6 @@
 
 apt-get update -y
 apt-get install -y openmpi-bin
-apt-get install -y libboost-dev
-apt-get install -y libboost-mpi-dev
-apt-get install -y libboost-thread-dev
+apt-get install -y libboost-all-dev
 apt-get install -y build-essential
 apt-get install -y cmake
