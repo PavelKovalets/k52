83d3079ed504200ff6c4468b4e9454c7685c4b3b
Alexander Soroka <soroka.a.m@gmail.com>
2013-10-29 00:25:49 +0300

    Load config from xml file, merge configuration tree

--- include/k52/common/settings_manager.h
+++ include/k52/common/settings_manager.h
@@ -2,6 +2,7 @@
 #define SETTINGS_MANAGER_H
 
 #include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 
 namespace
 {
@@ -16,13 +17,22 @@ namespace k52
 class SettingsManager
 {
 public:
-    SettingsManager(int argument_count, char* argument_values[])
+    SettingsManager(int argument_count, const char* argument_values[])
     {
         ParseCommandLine(argument_count, argument_values);
     }
 
+    // Load config file
+    SettingsManager(const std::string& config_filename)
+    {
+        if (!ParseXmlConfigFile(config_filename))
+        {
+            std::cerr << "Configuration file is not supported" << std::endl;
+        }
+    }
+
     // Initialize from command line
-    void ParseCommandLine(int argument_count, char* argument_values[])
+    void ParseCommandLine(int argument_count, const char* argument_values[])
     {
         for (size_t i = 0; i < argument_count; ++i)
         {
@@ -39,17 +49,85 @@ public:
         }
     }
 
-    // For mandatory parameters
-    template <class T>
-    const T& get(const std::string& property_name) const
+    // Initialize from configuration xml
+    bool ParseXmlConfigFile(const std::string& filename)
+    try
     {
-        return T();
+        boost::property_tree::xml_parser::read_xml(filename, property_tree_);
+        return true;
+    }
+    catch (...)
+    {
+        std::cerr << "Failed to parse configuration from xml file " << filename << std::endl;
+        return false;
+    }
+
+    // @note For mandatory property
+    // if property doesn't exist it will throw
+    template <class T>
+    T get(const std::string& property_name) const
+    try
+    {
+        return property_tree_.get<T>(property_name);
+    }
+    catch (...)
+    {
+        std::cerr << "Failed to get mandatory property " << property_name <<std::endl;
+
+        // There is no way to recover missed mandatory property,
+        // so just re-throw it to the caller
+        // TODO: define missed-mandatory-parameter exception
+        throw;
     }
 
     template <class T>
-    const T& get(const std::string& property_name, const T& default_value) const
+    T get(const std::string& property_name, const T& default_value) const
+    try
     {
-        return T();
+        return property_tree_.get<T>(property_name);
+    }
+    catch(...)
+    {
+        return default_value;
+    }
+
+    template <class T>
+    bool put(const std::string& property_name, const T& property_value)
+    try
+    {
+        property_tree_.put(property_name, property_value);
+        return true;
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+protected:
+    // @note Still not in boost :( - hope it will be added soon so implementation is fast,
+    // straightforward and not efficient
+    // Current implementation respects properties from the left tree - if the same property exists in both trees
+    // merged will contain value from the left one => Merge(l,r) != Merge(r,l)
+
+    static boost::property_tree::ptree Merge(const boost::property_tree::ptree& left_tree,
+                                             const boost::property_tree::ptree& right_tree)
+    {
+        boost::property_tree::ptree merged_tree(left_tree);
+        for(boost::property_tree::ptree::const_iterator it(right_tree.begin());
+            right_tree.end() != it; ++it)
+        {
+            boost::property_tree::ptree::iterator found(merged_tree.to_iterator(merged_tree.find(it->first)));
+            if (merged_tree.end() != found)
+            {
+                // Do not overload value of existed property, just add subtrees
+                merged_tree.get_child(it->first) = Merge(found->second, it->second);
+            }
+            else
+            {
+                merged_tree.put_child(it->first, it->second);
+            }
+        }
+        return merged_tree;
     }
 
 private:
