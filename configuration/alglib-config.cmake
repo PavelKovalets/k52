b4b66686dd5c20a68376152c098d97e927bfda9d
Pavel Kovalets <feanor-pk@yandex.ru>
2014-03-24 23:44:03 +0300

    fix some dependancies in cmake config

--- configuration/alglib-config.cmake
+++ configuration/alglib-config.cmake
@@ -1,9 +1,9 @@
 if(ALGLIB_ROOT)
-  message("Using custom alglib root path : ${ALGLIB_ROOT}")
+  message("${K52_MESSAGE_PREFIX} Using custom alglib root path : ${ALGLIB_ROOT} ${K52_MESSAGE_POSTFIX}")
   find_library(ALGLIB_FOUND alglib ${ALGLIB_ROOT}/lib)
 
   if(ALGLIB_FOUND)
-    message("Found library [alglib]")
+    message("${K52_MESSAGE_PREFIX} Found library [alglib] ${K52_MESSAGE_POSTFIX}")
     add_definitions(-DBUILD_WITH_ALGLIB)
     set(K52_ALGLIB_INCLUDE_DIRS  ${ALGLIB_ROOT}/cpp/src)
     set(K52_ALGLIB_LIBRARY_DIRS  ${ALGLIB_ROOT}/lib)
