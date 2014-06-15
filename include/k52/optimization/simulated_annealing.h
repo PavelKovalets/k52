d0820081a7ad23a21a55bc8147d74abe7e057584
Pavel Kovalets <feanor-pk@yandex.ru>
2014-06-15 13:47:42 +0300

    add simulated annealing files (see #37)

---
+++ include/k52/optimization/simulated_annealing.h
@@ -0,0 +1,28 @@
+#ifndef SIMULATEDANNEALING_H_
+#define SIMULATEDANNEALING_H_
+
+#include <k52/common/disallow_copy_and_assign.h>
+#include <k52/optimization/continuous_optimizer.h>
+
+namespace k52
+{
+namespace optimization
+{
+
+class SimulatedAnnealing : ContinuousOptimizer
+{
+public:
+    SimulatedAnnealing() {}
+
+protected:
+    virtual std::vector<double> FindOptimalParameters(const std::vector<double>& initial_values);
+
+private:
+
+    DISALLOW_COPY_AND_ASSIGN(SimulatedAnnealing);
+};
+
+}/* namespace optimization */
+}/* namespace k52 */
+
+#endif /* SIMULATEDANNEALING_H_ */
---
+++ src/optimization/simulated_annealing.cpp
@@ -0,0 +1,19 @@
+#include <k52/optimization/simulated_annealing.h>
+
+using ::std::vector;
+
+namespace k52
+{
+namespace optimization
+{
+
+vector<double> SimulatedAnnealing::FindOptimalParameters(const vector<double>& initial_values)
+{
+    vector<double> optimal_values = initial_values;
+
+
+    return optimal_values;
+}
+
+}/* namespace optimization */
+}/* namespace k52 */
