0350a138708008499740ea2b6798e1585cd7e683
Alexander Soroka <soroka.a.m@gmail.com>
2015-12-26 04:41:16 +0300

    Implemented funtional fourier test based on k52 thread poolw!

--- src/tests/unit_tests/dsp/fourier_functional.test.cpp
+++ src/tests/unit_tests/dsp/fourier_functional.test.cpp
@@ -1,9 +1,12 @@
 #include <boost/test/unit_test.hpp>
-#include <boost/thread.hpp>
+
+#ifdef BUILD_WITH_BOOST_THREAD
 
 #include <k52/common/constants.h>
 #include <k52/dsp/transform/i_fourier_transform.h>
 #include <k52/dsp/transform/fast_fourier_transform.h>
+#include <k52/parallel/i_worker_pool.h>
+#include <k52/parallel/worker_pool_factory.h>
 
 void complex_harmonic_transform(const k52::dsp::IFourierTransform* ft)
 {
@@ -25,18 +28,38 @@ void complex_harmonic_transform(const k52::dsp::IFourierTransform* ft)
 
 BOOST_AUTO_TEST_SUITE(fourier_transform_functional_tests);
 
-void test_function ()
+class TestTaskResult : public k52::parallel::ITaskResult
 {
-    k52::dsp::FastFourierTransform fft(10000000);
-    complex_harmonic_transform(&fft);
-}
+};
+
+class TestTask : public k52::parallel::ITask
+{
+public:
+    k52::parallel::ITaskResult* Perform() const
+    {
+        k52::dsp::FastFourierTransform fft(128);
+        complex_harmonic_transform(&fft);
+
+        return new TestTaskResult();
+    }
+};
 
 BOOST_AUTO_TEST_CASE(multithread_test)
 {
+    k52::parallel::IWorkerPool::shared_ptr pool =
+            k52::parallel::WorkerPoolFactory::CreateWorkerPool(k52::parallel::WorkerPoolFactory::kThreadWorkerPool);
+
+    std::vector<k52::parallel::ITask::shared_ptr> tasks;
+    std::vector<const k52::parallel::ITask*> tasks_pointers;
     for (int i = 0; i < 10; ++i)
     {
-        boost::thread th(test_function);
+        tasks.push_back(k52::parallel::ITask::shared_ptr(new TestTask()));
+        tasks_pointers.push_back(tasks[i].get());
     }
+
+    pool->DoTasks(tasks_pointers);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
+
+#endif
