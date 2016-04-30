f66746a678b044ef5d1e093745136d5dd4e9a326
sever90b41v <anonimanetu@mail.ru>
2016-04-30 18:08:09 +0300

    Hooke-Jeeves optimization method added

---
+++ include/k52/optimization/hooke_jeeves_method.h
@@ -0,0 +1,90 @@
+#ifndef HOOKE_JEEVES_METHOD_H_
+#define HOOKE_JEEVES_METHOD_H_
+
+#include <boost/shared_ptr.hpp>
+
+#include <k52/common/constants.h>
+#include <k52/optimization/continuous_optimizer.h>
+
+namespace k52
+{
+namespace optimization
+{
+
+//sourses:	http://elib.bsu.by/handle/123456789/8648
+//DOC5.NB  ||  MVE - chast'2-2006.ppt
+//alternative link:
+//https://drive.google.com/folderview?id=0B6NiSXfI55hTMjhTTF85bHpyYzQ&usp=drive_web&tid=0B6NiSXfI55hTSWpYV0g2Z25PejA#list	
+//DOC5.pdf
+
+/// @class HookeJeevesMethod
+/// @brief Realization of Hooke-Jeeves optimization method.
+class HookeJeevesMethod: public ContinuousOptimizer
+{
+public:
+    /// @brief Creates instance of HookeJeevesMethod.
+    /// @param acceleration - number defines value of increase for step value for better convergence.
+    /// Some researches have shown that minimum number of iterations will be achieved by coefficient that equals 3.5.
+    /// @param init_step - number that defines value of step on first iteration.
+    /// @param max_iteration_number - maximum number of iterations before computations will be stopped.
+    /// @param precision - number that defines accuracy of our computations.
+    /// @param step_divider - number that uses for decrease step value during search of direction.
+    HookeJeevesMethod(
+        double acceleration = 3.5,
+        double init_step = 1,
+        size_t max_iteration_number = 10000,
+        double precision = common::Constants::Eps,
+        double step_divider = 10 );
+
+    /// @brief  See ICloneable.
+    /// Creates deep clone of an object with resource allocation.
+    /// @return deep clone of an object.
+    virtual HookeJeevesMethod* Clone() const;
+
+    virtual std::string get_name() const;
+
+#ifdef BUILD_WITH_MPI
+    virtual void Send(boost::mpi::communicator* communicator, int target) const;
+
+    virtual void Receive(boost::mpi::communicator* communicator, int source);
+#endif
+
+protected:
+    virtual std::vector<double> FindOptimalParameters(const std::vector<double>&);
+
+    /// @brief Checks if steps vector length satisfies an exit criteria.
+    /// @param steps_array - vector of steps values for diffirent coordinates. 
+    /// @return true if steps vector norm less than specified precision, false other way.
+    bool IsExitCriteriaFulfilled(
+        const std::vector<double>& steps_array) const;
+
+    /// @brief Realization of Coordinate wise search.
+    /// Method implements search from start point in all directions with specified step value.
+    /// Hooke-Jeeves method is based on this optimization method.
+    /// @param arguments - vector of coordinates of start point.
+    /// @param steps_array - vector of values of step for different coordinates.
+    /// @return vector of new coordinates.
+    std::vector<double> CoordinatewiseSearch(
+        const std::vector<double>& arguments,
+        const std::vector<double>& steps_array) const;
+
+private:
+    /// Multiplier that increases step value for better convergence.
+    double acceleration_;
+
+    /// First step by all of coordinats.
+    double init_step_;
+
+    /// Maximum iterations number.
+    size_t max_iteration_number_;
+
+    /// Used precision.
+    double precision_;
+
+    /// Divider that uses for decrease step value during search of direction.
+    double step_divider_;
+};
+
+}// namespace optimization
+}// namespace k52
+#endif
--- src/common/floating_point.cpp
+++ src/common/floating_point.cpp
@@ -15,5 +15,25 @@ bool FloatingPoint::IsZero(double x)
     return AreEqual(x, 0);
 }
 
+bool FloatingPoint::AreEqual(
+    const std::vector<double>& vector1,
+    const std::vector<double>& vector2)
+{
+    if (vector1.size() != vector2.size())
+    {
+        return false;
+    }
+
+    for (size_t i = 0; i < vector1.size(); ++i)
+    {
+        if (!AreEqual(vector1[i], vector2[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 }/* namespace common */
 }/* namespace k52 */
