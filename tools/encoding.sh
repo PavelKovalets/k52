e393aa30062a2d42c99bce1bfd4a6788dc0afce0
Pavel Kovalets <kovalets.p.e@hotmail.com>
2016-06-10 18:25:11 +0300

    add encoding check to CI

--- tools/encoding.sh
+++ tools/encoding.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-cd ..
+SCRIPT_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
+TOP_DIR="${SCRIPT_DIR}/.."
 
-find . -type f \( -name "*.cpp" -or -name "*.h" -or -name "*.txt" -or -name "*.sh" -or -name "*.cmake" -or -name "*.inl" -or -name "*.hpp" \) -exec bash -c '
+cd ${TOP_DIR}
 
-if ! (enca "{}" -L russian | grep -q "7bit ASCII characters") ; then
-    echo "File '{}'"
-    enca "{}" -L russian
-fi 
+RESULT=0
 
-' -- {} \;
+for text_file in $(find . -type f \( -name "*.cpp" -or -name "*.h" -or -name "*.txt" -or -name "*.sh" -or -name "*.cmake" -or -name "*.inl" -or -name "*.hpp" \));
+do
+    if ! (enca $text_file -L russian | grep -q "7bit ASCII characters") ; then
+        echo "File $text_file"
+        enca $text_file -L russian
+        RESULT=1
+    fi 
+done
 
+if [[ $RESULT = 1 ]]; then
+    echo "ERROR: Some files have content encoded differently  than 7bit ASCII."
+    exit 1
+fi
 
+exit 0
